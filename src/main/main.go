463c202bb916c1c632bcdac66aed36115427264c
Jake Bailey <jake.bailey2801@gmail.com>
2020-03-15 14:29:17 +0000

    Query & URL Parameters

--- src/main/main.go
+++ src/main/main.go
@@ -10,12 +10,35 @@ func start(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from the website")
 }
 
+func getCats(c echo.Context) error{
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("Your cat name is: %s\nand the type is %s\n", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to let us know if you want json or string data",
+	})
+}
+
 func main() {
 	fmt.Println("Welcome to the server")
 
 	e := echo.New()
 
 	e.GET("/", start)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":8000")
 }
